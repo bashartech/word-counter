5268ad65b5291278b2cfe3deaf9cbc9411b08c04
bashartech <144168218+bashartech@users.noreply.github.com>
2024-04-17 02:33:12 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+// This line is for shebang and it tells the operating system to run node.js
+
+import inquirer from "inquirer"
+
+//Declare a constant "answers" aand assign it to the result of inquirer.prompt funtion
+
+const answers : {
+    Sentence : any
+} = await inquirer.prompt([
+    {
+    name: "Sentence",
+    type:"input",
+    message : "Enter your sentence to count the word"
+   }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+// Print the array of words to the console
+
+console.log(words)
+
+// print the word
+
+console.log(`Your sentence word count is ${words.length}`);
